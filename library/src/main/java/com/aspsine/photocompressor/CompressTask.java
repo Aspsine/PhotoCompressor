308ec4fb2c2753005415974649b36b30d6cac14e
Aspsine <littleximail@gmail.com>
2016-01-16 22:33:16 +0800

    clean up

--- library/src/main/java/com/aspsine/photocompressor/CompressTask.java
+++ library/src/main/java/com/aspsine/photocompressor/CompressTask.java
@@ -6,10 +6,8 @@ import android.os.Build;
 import android.text.TextUtils;
 import android.util.Log;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
--- library/src/main/java/com/aspsine/photocompressor/MemoryCache.java
+++
@@ -1,8 +0,0 @@
-package com.aspsine.photocompressor;
-
-
-/**
- * Created by aspsine on 16/1/12.
- */
-public class MemoryCache {
-}
